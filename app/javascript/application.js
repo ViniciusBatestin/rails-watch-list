63c4e52a53d4fb67f0fa6d09258c755b3ed9a335
Vinicius Batestin <viniciusbatestin@gmail.com>
2023-02-08 22:36:31 +0100

    models

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,15 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap";
+
+
+
+// Associations
+
+//     A list has many bookmarks
+//     A list has many movies through bookmarks
+//     A movie has many bookmarks
+//     A bookmark belongs to a movie
+//     A bookmark belongs to a list
+//     You can’t delete a movie if it is referenced in at least one bookmark.
+//     When you delete a list, you should delete all associated bookmarks (but not the movies as they can be referenced in other lists).
